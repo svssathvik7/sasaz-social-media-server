1936370f044741d8af66ae137fe4f71c8c9e90da
SATHVIK SVS <svssathvik77@gmail.com>
2023-12-06 11:25:18 +0530

    database connected

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,3 +1,8 @@
 const mongoose = require("mongoose");
-const db = 1;
+// process.env will be used once we move to production
+mongoose.connect("mongodb+srv://sathishsara1007:PUTbv0P7vPmJQniW@cluster0.4nso06z.mongodb.net/?retryWrites=true&w=majority");
+const db = mongoose.connection
+db.on("error",()=>{console.log("Error connection to db")});
+db.once("open",()=>{console.log("Successfully connected to database")});
+
 module.exports = db;
