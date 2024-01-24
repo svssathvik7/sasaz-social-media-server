3643605102fec9b76d6de30cd9366b5fa94eb7bf
SATHVIK SVS <svssathvik77@gmail.com>
2024-01-24 19:36:50 +0530

    explore feed complete

--- api/userUploads.js
+++ api/userUploads.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const Router = express.Router();
 const { userPost, getUserDetails, getUserPosts, userComment, userLike, getAllUserDetails, getAllUserPosts ,manageUserFrnds,deletePost} = require("../controllers/userUploadsController");
+const {exploreFeed} = require("../controllers/postFeed");
 
 Router.post("/userPost", userPost);
 Router.post('/getUserDetails', getUserDetails);
@@ -11,4 +12,5 @@ Router.get('/allUsers', getAllUserDetails);
 Router.post("/getUserPosts", getUserPosts);
 Router.post("/getAllPosts",getAllUserPosts);
 Router.post("/deletePost",deletePost);
+Router.get("/exploreFeed",exploreFeed);
 module.exports = Router;
---
+++ controllers/postFeed.js
@@ -0,0 +1,17 @@
+// This will mostly be replaced with recommendor system so this has to be a separate controller
+const postModel = require("../models/postModel");
+const categories = ["Study","Space","Fashion"]
+const exploreFeed = async (req,res)=>{
+    try {
+        const cats = await postModel.distinct("category");
+        const segregatedPosts = [];
+        for(const cat of cats){
+            const temp = await postModel.find({category:cat}).limit(15);
+            segregatedPosts.push(temp);
+        }
+        res.json({message:"Success!",status:true,posts:segregatedPosts});
+    } catch (error) {
+        res.json({message:"Failed getting data!",status:false});
+    }
+}
+module.exports = {exploreFeed}
