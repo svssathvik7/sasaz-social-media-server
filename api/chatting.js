2975780db04164d9b0aab7533ecce20f0fa99b98
Azeem Shaik <azeemshaik025@gmail.com>
2024-01-27 18:48:31 +0530

    Create Emoji Reactions for Messages

--- api/chatting.js
+++ api/chatting.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const Router = express.Router();
-const {fetchMessages} = require("../controllers/chatController");
-const {addMessage} = require("../controllers/chatController");
-Router.post("/messages/",fetchMessages);
-Router.post("/addMessage/",addMessage);
+const { fetchMessages, reactMessage } = require("../controllers/chatController");
+const { addMessage } = require("../controllers/chatController");
+Router.post("/messages/", fetchMessages);
+Router.post("/addMessage/", addMessage);
+Router.post('/reactMessage', reactMessage);
 module.exports = Router;
--- models/chatModel.js
+++ models/chatModel.js
@@ -1,31 +1,32 @@
 const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema({
-    user : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : 'users'
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users'
     },
-    message : {
-        type : String,
-        required : true
+    message: {
+        type: String,
+        required: true
     },
-    timeStamp : {
-        type : Date,
-        default : Date.now
+    reply: String,
+    timeStamp: {
+        type: Date,
+        default: Date.now
     }
 });
 
 const chatSchema = new mongoose.Schema({
-    id : {
-        type : String,
-        required : true
+    id: {
+        type: String,
+        required: true
     },
-    chat : [{
-        type : mongoose.Schema.Types.ObjectId,
-        ref : 'messages'
+    chat: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'messages'
     }]
 });
-const messageDb = new mongoose.model("messages",messageSchema);
-const chatDb = new mongoose.model("chats",chatSchema);
- 
-module.exports = {messageDb,chatDb};
+const messageDb = new mongoose.model("messages", messageSchema);
+const chatDb = new mongoose.model("chats", chatSchema);
+
+module.exports = { messageDb, chatDb };
